71edf9021cf2715b41b23a06879d1d7f3c84a134
dadi <18739961091@163.com>
2019-06-09 20:26:12 +0800

    在helloGit中添加一句话--创建了git工程,现在准备把他上传到github上

--- helloGit/src/com/dadi/GitDemo.java
+++ helloGit/src/com/dadi/GitDemo.java
@@ -3,5 +3,6 @@ package com.dadi;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("创建了一个");
+        System.out.println("创建了git工程,现在准备把他上传到github上");
     }
 }
